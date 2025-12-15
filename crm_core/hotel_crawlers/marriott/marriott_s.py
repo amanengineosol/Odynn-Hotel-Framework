4ee89e45abfaa4437584943fdcdc24a76883d698
Amod Mishra <amod.mishra@engineosol.com>
2025-12-15 14:56:02 +0530

    changes reverted of  cdp logs

--- crm_core/hotel_crawlers/marriott/marriott_s.py
+++ crm_core/hotel_crawlers/marriott/marriott_s.py
@@ -62,7 +62,7 @@ mycdp.util.parse_json_event = patched_parse_event
 # --- SETUP LOGGING ---
 # Configure the logger for the module
 logger = logging.getLogger('MarriottScraper')
-logger.setLevel(logging.ERROR)
+logger.setLevel(logging.INFO)
 logger.propagate = False
 
 # Create console handler and set level to debug
